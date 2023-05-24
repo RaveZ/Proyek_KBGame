8750eed9dbb0f7111cc1683ab2c90a8dd494812c
SebastianJonathan <banfuk.rong@gmail.com>
2023-05-24 12:52:48 +0700

    Porjek MLsatu Sebastian Jonathan

---
+++ Assets/cobaGoal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+public class cobaGoal : Agent
+{
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        base.OnActionReceived(actions);
+    }
+
+
+}
